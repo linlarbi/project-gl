7f488dcdf933f89c20ce0806c710f8d2bc7ab347
linlarbi <larbi.lina2023@gmail.com>
2025-03-25 00:16:44 +0100

    initialisation project gl

---
+++ src/test/java/com/usthb/gl/GlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usthb.gl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
